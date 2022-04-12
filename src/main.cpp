f62ef9a7c7f9b7538c28c946636e0a3839fb873c
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-04-12 11:58:04 -0300

    fonctionnemment du circuit

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,39 @@
 
 #include <Arduino.h>
 #include <Keypad.h>
-#include "WIFIConnector_MKR1000.h"
-#include "MQTTConnector.h"
+#include <Wire.h>
 
 
-void setup() {
-  // put your setup code here, to run once:
+#define ROWS 4
+#define COLS 4
+
+// Declaration des variables
+
+const char kp4x4Keys[ROWS][COLS] = {{'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9', 'C'}, {'*', '0', '#', 'D'}};
+byte rowKp4x4Pin[4] = {9, 8, 7, 6};
+byte colKp4x4Pin[4] = {5, 4, 3, 2};
+
+Keypad kp4x4 = Keypad(makeKeymap(kp4x4Keys), rowKp4x4Pin, colKp4x4Pin, ROWS, COLS);
+char boutton;
+// fonctions setup
+void setup()
+{
+    Serial.begin(9600);
+    Wire.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// developpement des fonctions
+void loop()
+{
+
+    // choix de la LED
+    boutton = kp4x4.getKey();
+
+    if (boutton) {
+        Serial.println(boutton);
+        // Envoie de la lettre à l'autre uC
+        Wire.beginTransmission(7);
+        Wire.write(boutton);
+        Wire.endTransmission();
+    }
 }
