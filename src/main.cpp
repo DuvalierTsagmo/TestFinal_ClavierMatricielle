16dac80ba9ef71ddc195db7dbbdaeef50c305c1d
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-04-12 11:05:36 -0300

    fonctionnemment du circuit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*
+  Titre      : Clavier Matricielle
+  Auteur     : Duvalier Tsagmo
+  Date       : 12/04/2022
+  Description: controler un servo moteur avec un clavier maticielle
+  Version    : 0.0.1
+*/
+
+
+#include <Arduino.h>
+#include <Keypad.h>
+#include "WIFIConnector_MKR1000.h"
+#include "MQTTConnector.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
